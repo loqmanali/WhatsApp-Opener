37ef0baee65b7792e453b63b1e2a4bddc5e15450
loqman <59010033+loqmanali@users.noreply.github.com>
2024-09-08 13:43:58 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e88b7e5cf2336b3e9c2c24c18a2fa10c",
+const RESOURCES = {"flutter_bootstrap.js": "e551231eac38a95a96cc3be6fc28f911",
 "version.json": "a9f8c184afca332b3c9d84e7f9c7128a",
-"index.html": "0b961e8f567bac7d58baadd9188596df",
-"/": "0b961e8f567bac7d58baadd9188596df",
-"main.dart.js": "042e053f82cd381eef9640837a73e83e",
+"index.html": "f81e230f96acf36a446511e0aea74aa7",
+"/": "f81e230f96acf36a446511e0aea74aa7",
+"main.dart.js": "dc64c913653aa8766242a1898a4e4977",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
